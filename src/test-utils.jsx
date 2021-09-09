1c59ac344eb10295b6b23a22aff4c4ecb3027f5f
Lameck1 <lameck72@gmail.com>
2021-09-09 15:29:45 +0300

    Add test utils

---
+++ src/test-utils.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { render as rtlRender } from '@testing-library/react';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import coinsReducer from './redux/coins/coins';
+
+function render(
+  ui,
+  {
+    preloadedState,
+    store = configureStore({
+      reducer: {
+        missions: coinsReducer,
+      },
+      preloadedState,
+    }),
+    ...renderOptions
+  } = {},
+) {
+  // eslint-disable-next-line react/prop-types
+  function Wrapper({ children }) {
+    return <Provider store={store}>{children}</Provider>;
+  }
+  return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
+}
+
+export * from '@testing-library/react';
+export { render };
