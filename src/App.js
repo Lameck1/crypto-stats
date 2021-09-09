39474fd0fa7b5f5608a56499e1f58b6d0f0aabc1
Lameck1 <lameck72@gmail.com>
2021-09-09 15:10:13 +0300

    Refactor App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import Home from './components/home/Home';
+import Details from './components/details/Details';
+import { store, persistor } from './redux/configureStore';
 import './App.css';
 
-function App() {
-  return (
-    <h1>Hello Crypto Stats</h1>
-  );
-}
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: <Home />,
+  },
+  {
+    path: '/coins/:id',
+    name: 'Coins',
+    component: <Details />,
+  },
+];
+
+const App = () => (
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <Router>
+        <Switch>
+          {routes.map(({ path, component }) => (
+            <Route path={path} exact key={path}>{component}</Route>
+          ))}
+        </Switch>
+      </Router>
+    </PersistGate>
+  </Provider>
+);
 
 export default App;
