b230478965b26b7bebc7fc54a868d3e1626c1be5
Lameck1 <lameck72@gmail.com>
2021-09-11 02:31:01 +0300

    Refactor reducer to use RTK

--- src/redux/coins/coins.js
+++ src/redux/coins/coins.js
@@ -1,39 +1,72 @@
-import fetchCoins from './coinStats';
+/* eslint-disable no-param-reassign */
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { fetchCoin, fetchCoins } from './coinStats';
 
-const LOAD_COINS = 'crypto-stats/load-coins';
-
-export const loadCoins = (payload) => ({
-  type: LOAD_COINS,
-  payload,
+export const getCoinById = createAsyncThunk('coins/getCoinById', async (id) => {
+  const selected = await fetchCoin(id);
+  return selected.map(({
+    id, icon, name, rank, price: priceInUSD, priceBtc: priceInBitcoin,
+    volume, marketCap, priceChange1h: hourlyPriceChange,
+  }) => ({
+    id,
+    icon,
+    name,
+    rank,
+    priceInUSD,
+    priceInBitcoin,
+    volume,
+    marketCap,
+    hourlyPriceChange,
+  }));
 });
 
-export const getCoins = async (dispatch) => {
+export const getCoins = createAsyncThunk('coins/getCoins', async () => {
   const coins = await fetchCoins();
-  if (coins) {
-    dispatch(loadCoins(coins.map(({
-      id, icon, name, rank, price: priceInUSD, priceBtc: priceInBitcoin,
-      volume, marketCap, priceChange1h: hourlyPriceChange,
-    }) => ({
-      id,
-      icon,
-      name,
-      rank,
-      priceInUSD,
-      priceInBitcoin,
-      volume,
-      marketCap,
-      hourlyPriceChange,
-    }))));
-  }
-};
+  return coins.map(({
+    id, icon, name, rank, price: priceInUSD, priceBtc: priceInBitcoin,
+    volume, marketCap, priceChange1h: hourlyPriceChange,
+  }) => ({
+    id,
+    icon,
+    name,
+    rank,
+    priceInUSD,
+    priceInBitcoin,
+    volume,
+    marketCap,
+    hourlyPriceChange,
+  }));
+});
 
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case LOAD_COINS:
-      return action.payload;
-    default:
-      return state;
-  }
-};
+const coinsSlice = createSlice({
+  name: 'coins',
+  initialState: {
+    coins: [],
+    status: null,
+    selected: [],
+  },
+  extraReducers: {
+    [getCoinById.pending]: (state) => {
+      state.status = 'loading';
+    },
+    [getCoinById.fulfilled]: (state, { payload }) => {
+      state.selected = payload;
+      state.status = 'success';
+    },
+    [getCoinById.rejected]: (state) => {
+      state.status = 'failed';
+    },
+    [getCoins.pending]: (state) => {
+      state.status = 'loading';
+    },
+    [getCoins.fulfilled]: (state, { payload }) => {
+      state.coins = payload;
+      state.status = 'success';
+    },
+    [getCoins.rejected]: (state) => {
+      state.status = 'failed';
+    },
+  },
+});
 
-export default reducer;
+export default coinsSlice.reducer;
