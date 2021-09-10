990d0ffa54678c04582a0aa63edffed6383d211f
Lameck1 <lameck72@gmail.com>
2021-09-11 02:30:25 +0300

    Add fetchCoin method

--- src/redux/coins/coinStats.js
+++ src/redux/coins/coinStats.js
@@ -1,14 +1,21 @@
-const COINS_URL = 'https://api.coinstats.app/public/v1/coins';
-
-const fetchCoins = async () => {
+export const fetchCoins = async () => {
   const requestOptions = {
     method: 'GET',
     redirect: 'follow',
   };
-  const response = await fetch(COINS_URL, requestOptions);
+  const response = await fetch('https://api.coinstats.app/public/v1/coins', requestOptions);
   const result = await response.json();
   const { coins } = result;
   return coins;
 };
 
-export default fetchCoins;
+export const fetchCoin = async (id) => {
+  const requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+  };
+  const response = await fetch(`https://api.coinstats.app/public/v1/coins/${id}`, requestOptions);
+  const result = await response.json();
+  const { coin } = result;
+  return [coin];
+};
