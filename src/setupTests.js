6d55474126abf0806f493b20bc4eef0d200c7de2
Lameck1 <lameck72@gmail.com>
2021-09-11 03:32:49 +0300

    Refactor test setup

--- src/setupTests.js
+++ src/setupTests.js
@@ -6,11 +6,20 @@ import '@testing-library/jest-dom/extend-expect';
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
 
-const { getComputedStyle } = window;
-window.getComputedStyle = (elt) => getComputedStyle(elt);
-
 const handlers = [
   rest.get('https://api.coinstats.app/public/v1/coins', (req, res, ctx) => res(ctx.json([{
+    id: 'bitcoin',
+    icon: 'https://static.coinstats.app/coins/Bitcoin6l39t.png',
+    name: 'Bitcoin',
+    symbol: 'BTC',
+    rank: 1,
+    price: 46244.766908363454,
+    priceBtc: 1,
+    volume: 46924318377.192154,
+    marketCap: 869906518242.3385,
+    priceChange1h: -0.09,
+  }]), ctx.delay(150))),
+  rest.get('https://api.coinstats.app/public/v1/coins/bitcoin', (req, res, ctx) => res(ctx.json([{
     id: 'bitcoin',
     icon: 'https://static.coinstats.app/coins/Bitcoin6l39t.png',
     name: 'Bitcoin',
