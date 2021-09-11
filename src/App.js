ed8fe2f41f0fc1322cfb9f2516b4a20c40ae3506
Lameck1 <lameck72@gmail.com>
2021-09-11 03:31:37 +0300

    Remove redux-persist

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
 import Home from './components/home/Home';
 import Details from './components/details/Details';
-import { store, persistor } from './redux/configureStore';
+import store from './redux/configureStore';
 import './App.css';
 
 const routes = [
@@ -22,15 +21,13 @@ const routes = [
 
 const App = () => (
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <Router>
-        <Switch>
-          {routes.map(({ path, component }) => (
-            <Route path={path} exact key={path}>{component}</Route>
-          ))}
-        </Switch>
-      </Router>
-    </PersistGate>
+    <Router>
+      <Switch>
+        {routes.map(({ path, component }) => (
+          <Route path={path} exact key={path}>{component}</Route>
+        ))}
+      </Switch>
+    </Router>
   </Provider>
 );
 
