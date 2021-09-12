80fa16cd0e668154cfc89a3642322607e6c20fa8
Lameck1 <lameck72@gmail.com>
2021-09-12 23:47:01 +0300

    Set up tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -6,31 +6,37 @@ import '@testing-library/jest-dom/extend-expect';
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
 
+const { getComputedStyle } = window;
+window.getComputedStyle = (elt) => getComputedStyle(elt);
+
 const handlers = [
-  rest.get('https://api.coinstats.app/public/v1/coins', (req, res, ctx) => res(ctx.json([{
-    id: 'bitcoin',
-    icon: 'https://static.coinstats.app/coins/Bitcoin6l39t.png',
-    name: 'Bitcoin',
-    symbol: 'BTC',
-    rank: 1,
-    price: 46244.766908363454,
-    priceBtc: 1,
-    volume: 46924318377.192154,
-    marketCap: 869906518242.3385,
-    priceChange1h: -0.09,
-  }]), ctx.delay(150))),
-  rest.get('https://api.coinstats.app/public/v1/coins/bitcoin', (req, res, ctx) => res(ctx.json([{
-    id: 'bitcoin',
-    icon: 'https://static.coinstats.app/coins/Bitcoin6l39t.png',
-    name: 'Bitcoin',
-    symbol: 'BTC',
-    rank: 1,
-    price: 46244.766908363454,
-    priceBtc: 1,
-    volume: 46924318377.192154,
-    marketCap: 869906518242.3385,
-    priceChange1h: -0.09,
-  }]), ctx.delay(150))),
+  rest.get('https://api.coinstats.app/public/v1/coins', (req, res, ctx) => res(ctx.json({
+    coins: [
+      {
+        id: 'bitcoin',
+        icon: 'https://static.coinstats.app/coins/Bitcoin6l39t.png',
+        name: 'Bitcoin',
+        rank: 1,
+        price: 45137.11560416475,
+        priceBtc: 1,
+        volume: 29984546496.858982,
+        marketCap: 849187666064.3407,
+        priceChange1h: -0.53,
+      }],
+  }), ctx.delay(150))),
+  rest.get('https://api.coinstats.app/public/v1/coins/bitcoin', (req, res, ctx) => res(ctx.json({
+    coin: {
+      id: 'bitcoin',
+      icon: 'https://static.coinstats.app/coins/Bitcoin6l39t.png',
+      name: 'Bitcoin',
+      rank: 1,
+      price: 45137.11560416475,
+      priceBtc: 1,
+      volume: 29984546496.858982,
+      marketCap: 849187666064.3407,
+      priceChange1h: -0.53,
+    },
+  }), ctx.delay(150))),
 ];
 
 const server = setupServer(...handlers);
